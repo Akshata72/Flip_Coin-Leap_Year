acfe9612b79bbb6840a68a82fa452561045beff5
Akshata72 <sableakshata880@gmail.com>
2022-03-05 18:10:09 +0530

    Added;Flip_Coin-Leap_Year

---
+++ flip coin/flip coin/Program.cs
@@ -0,0 +1,13 @@
+﻿int Coin_Head = 1;
+Random random = new Random();
+int coincheck = random.Next(0, 2);
+if(coincheck == Coin_Head)
+{
+    Console.WriteLine("Coin is Head");
+
+}
+else
+{
+    Console.WriteLine("Coin is Tail");
+
+}
